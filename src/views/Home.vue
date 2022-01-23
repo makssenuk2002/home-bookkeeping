75b3425def5fa02349a661dcebafc3c604e44396
Maks <makssenuk2002@gmail.com>
2022-01-23 11:27:53 +0200

    finall

---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div   class="row">
+     <History/>
+    </div>
+  </div>
+</template>
+
+<script>
+import HomeBill from '@/components/HomeBill'
+import HomeCurrency from '@/components/HomeCurrency'
+import History from "./History";
+
+export default {
+  name: 'home',
+  metaInfo() {
+    return {
+      title: this.$title('Menu_Bill')
+    }
+  },
+  data: () => ({
+    loading: true,
+    currency: null
+  }),
+  async mounted() {
+    this.currency = await this.$store.dispatch('fetchCurrency')
+    this.loading = false
+  },
+  methods: {
+    async refresh() {
+      this.loading = true
+      this.currency = await this.$store.dispatch('fetchCurrency')
+      this.loading = false
+    }
+  },
+  components: {
+    History,
+    HomeBill,
+    HomeCurrency
+  }
+}
+</script>
